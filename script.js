6bbd4b438056f96f0ce09a3c35fba6aec815adfb
Chris <mr.chrisnash@gmail.com>
2021-08-30 11:11:18 -0700

    getting data from weather api

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 console.log('hellloooo');
 
+displayWeather = document.querySelector('#currentWeather');
 
+async function getWeather(location){
+
+  try {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=42cf3a5858c2af79af82138fa993bc01&units=metric`, {mode: 'cors'});  
+    const data = await response.json();
+    console.log(data);
+    processWeatherData(data);
+    displayWeather.innerText = '...';
+  } catch(err) {
+    displayWeather.innerText = err;
+  }
+}
+
+// take weather data and only return required data
+function processWeatherData(data){
+
+  const feelsLike = data.main.feels_like;
+
+  console.log(feelsLike);
+
+}
